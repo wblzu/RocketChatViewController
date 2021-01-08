3c45bbecc1be431eed07f2bbe917939f5bcb967d
wblzu <125101391@qq.com>
2021-01-08 17:34:45 +0800

    Update BWComposerView.swift

--- Composer/Classes/Views/BWComposerView.swift
+++ Composer/Classes/Views/BWComposerView.swift
@@ -8,6 +8,12 @@
 import UIKit
 import Foundation
 
+public enum BWKeyboardStatus: String {
+    case BWLeft
+    case BWRight
+    case BWEditing
+}
+
 public protocol BWComposerViewDelegate: class {
     func leftButtonAction(_ composerView: BWComposerView)
     func rightButtonAction(_ composerView: BWComposerView)
@@ -20,15 +26,19 @@ public protocol BWComposerViewDelegate: class {
 public class BWComposerView: UIView {
     static let ObservingInputAccessoryViewFrameDidChangeNotification = "ObservingInputAccessoryViewFrameDidChangeNotification"
     
+    public let kTextViewDefaultHeight: CGFloat = 44
+    
     public var textView: UITextView!
     
+    public var containerView: UIView!
     public var leftButton: UIButton!
     public var rightButton: UIButton!
     
+    public var keyboardStatus: BWKeyboardStatus = .BWEditing
+    public var showArea: Bool = false
+    
     public weak var delegate: BWComposerViewDelegate?
     
-    public var leftButtonClicked: Bool = false
-    public var rightButtonClicked: Bool = false
     
     public convenience init() {
         self.init(frame: .zero)
@@ -46,20 +56,9 @@ public class BWComposerView: UIView {
 
     
     public func commonInit() {
-        if #available(iOS 13.0, *) {
-            let dyColor = UIColor { (trainCollection) -> UIColor in
-                if trainCollection.userInterfaceStyle == .light {
-                    return UIColor.white
-                } else {
-                    return UIColor.tertiarySystemBackground
-                }
-            }
-            self.backgroundColor = dyColor
-            
-        } else {
-            // Fallback on earlier versions
-            self.backgroundColor = UIColor.white
-        }
+        // backgroundView
+        let backgroundView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200))
+        addSubview(backgroundView)
 
         leftButton = UIButton(type: .system)
         leftButton.frame = CGRect(x: 10, y: 15, width: 35, height: 35)
@@ -75,44 +74,80 @@ public class BWComposerView: UIView {
         textView.delegate = self
         textView.font = UIFont.systemFont(ofSize: 17.0)
         
+        containerView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200))
+//        containerView.backgroundColor = .clear
+        containerView.backgroundColor = .orange
+        addSubview(containerView)
+        
+        let picButton = UIButton(type: .system)
+        picButton.frame = CGRect(x: 20, y: leftButton.frame.origin.y+leftButton.frame.size.height+30, width: 50, height: 50)
+        picButton.setImage(UIImage(named: "sharemore_pic"), for: .normal)
+        picButton.addTarget(self, action: #selector(action1(_:)), for: .touchUpInside)
+        
         addSubview(leftButton)
         addSubview(rightButton)
         addSubview(textView)
+        containerView.addSubview(picButton)
         
         if #available(iOS 13.0, *) {
             let dyColor1 = UIColor { (trainCollection) -> UIColor in
+                if trainCollection.userInterfaceStyle == .light {
+                    return UIColor.white
+                } else {
+                    return UIColor.tertiarySystemBackground
+                }
+            }
+            self.backgroundColor = dyColor1
+            backgroundView.backgroundColor = dyColor1
+            
+            let dyColor2 = UIColor { (trainCollection) -> UIColor in
                 if trainCollection.userInterfaceStyle == .light {
                     return UIColor(red: 243/255.0, green: 244/255.0, blue: 245/255.0, alpha: 1)
                 } else {
                     return UIColor.systemBackground
                 }
             }
-            textView.backgroundColor = dyColor1
+            textView.backgroundColor = dyColor2
             
         } else {
             // Fallback on earlier versions
+            self.backgroundColor = UIColor.white
+            backgroundView.backgroundColor = UIColor.white
             textView.backgroundColor = UIColor(red: 243/255.0, green: 244/255.0, blue: 245/255.0, alpha: 1)
         }
         
+        //
         textView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
     }
     
     @objc func action1(_ button: UIButton) {
-        if !leftButtonClicked {
-            leftButtonClicked = true
-            rightButtonClicked = false
+        if keyboardStatus != .BWLeft {
+            containerView.isHidden = true
+            keyboardStatus = .BWLeft
+            showArea = false
             textView.resignFirstResponder()
             delegate?.leftButtonAction(self)
         }
     }
     
     @objc func action2(_ button: UIButton) {
-        if !rightButtonClicked {
-            leftButtonClicked = false
-            rightButtonClicked = true
+        if keyboardStatus != .BWRight {
+            keyboardStatus = .BWRight
             textView.resignFirstResponder()
             delegate?.rightButtonAction(self)
+            containerView.isHidden = false
         }
+    }
+    
+    public override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        superview?.removeObserver(self, forKeyPath: "center")
+        
+        newSuperview?.addObserver(self, forKeyPath: "center", options: [.old, .new], context: nil)
+    }
+    
+    deinit {
+        superview?.removeObserver(self, forKeyPath: "center")
     }
     
     
@@ -132,7 +167,7 @@ public class BWComposerView: UIView {
             #if DEBUG
             let rect = self.superview!.frame
             let height = UIScreen.main.bounds.height-rect.minY-self.frame.height
-            if !rightButtonClicked {
+            if keyboardStatus != .BWRight {
                 delegate?.keyboardFrameChangeByDrag(textView, keyBoardheight: height)
             }
             print("observeValue self.superview!.frame.origin.y \(rect.origin.y) \(height)")
@@ -154,24 +189,13 @@ public class BWComposerView: UIView {
         textView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         textView.layoutManager.allowsNonContiguousLayout = false
     }
-    
-    public override func willMove(toSuperview newSuperview: UIView?) {
-        super.willMove(toSuperview: newSuperview)
-        superview?.removeObserver(self, forKeyPath: "center")
-        
-        newSuperview?.addObserver(self, forKeyPath: "center", options: [.old, .new], context: nil)
-    }
-    
-    deinit {
-        superview?.removeObserver(self, forKeyPath: "center")
-    }
 }
 
 
 extension BWComposerView: UITextViewDelegate {
     public func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
-        leftButtonClicked = false
-        rightButtonClicked = false
+        keyboardStatus = .BWEditing
+        showArea = false
         delegate?.textViewBeginEdit(textView)
         return true
     }
