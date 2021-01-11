8798cf42cb8c1c2d14ec52e8901ba67ab4f7b113
wblzu <125101391@qq.com>
2021-01-11 16:58:54 +0800

    Update BWComposerView.swift

--- Composer/Classes/Views/BWComposerView.swift
+++ Composer/Classes/Views/BWComposerView.swift
@@ -20,19 +20,24 @@ public protocol BWComposerViewDelegate: class {
     
     func textViewBeginEdit(_ textView: UITextView)
     func textViewTextChange(_ textView: UITextView)
-    func keyboardFrameChangeByDrag(_ textView: UITextView, keyBoardheight: CGFloat)
+    func keyboardFrameChange(_ textView: UITextView, keyBoardheight: CGFloat)
 }
 
 public class BWComposerView: UIView {
     static let ObservingInputAccessoryViewFrameDidChangeNotification = "ObservingInputAccessoryViewFrameDidChangeNotification"
     
     public let kTextViewDefaultHeight: CGFloat = 44.0
+    public let kTextViewMaxHeight: CGFloat = 122.0
+    public let kShowAreaHeight: CGFloat = 100.0
+    public var kLastTextViewFrame: CGRect = CGRect.zero
     
     public var textView: UITextView!
     
     public var containerView: UIView!
     public var leftButton: UIButton!
     public var rightButton: UIButton!
+    public var pressMic: UIView!
+    public var pressMicLabel: UILabel!
     
     public var keyboardStatus: BWKeyboardStatus = .BWEditing
     public var showArea: Bool = false
@@ -57,7 +62,7 @@ public class BWComposerView: UIView {
     
     public func commonInit() {
         // backgroundView
-        let backgroundView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200))
+        let backgroundView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: kShowAreaHeight))
         addSubview(backgroundView)
 
         leftButton = UIButton(type: .system)
@@ -70,17 +75,37 @@ public class BWComposerView: UIView {
         rightButton.setImage(UIImage(named: "发送文件"), for: .normal)
         rightButton.addTarget(self, action: #selector(action2(_:)), for: .touchUpInside)
         
+        
+        //
+        pressMic = UIView.init(frame: CGRect(x: 50, y: 10, width: UIScreen.main.bounds.width-100, height: kTextViewDefaultHeight))
+        pressMic.layer.cornerRadius = 10
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressRecognized(_:)))
+        longPress.minimumPressDuration = 0.01
+        pressMic.addGestureRecognizer(longPress)
+        addSubview(pressMic)
+        
+        pressMicLabel = UILabel.init(frame: CGRect(x: 0, y: 0, width: pressMic.frame.size.width, height: pressMic.frame.size.height))
+        pressMicLabel.isUserInteractionEnabled = false
+        pressMicLabel.textAlignment = .center
+        pressMicLabel.font = UIFont.systemFont(ofSize: 17.0, weight: .semibold)
+        pressMicLabel.text = "按住 说话"
+        pressMic.addSubview(pressMicLabel)
+        
+        
+        //
         textView = UITextView.init(frame: CGRect(x: 50, y: 10, width: UIScreen.main.bounds.width-100, height: kTextViewDefaultHeight))
         textView.delegate = self
         textView.font = UIFont.systemFont(ofSize: 17.0)
+        textView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
         
-        containerView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200))
+        containerView = UIView.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: kShowAreaHeight))
         containerView.backgroundColor = .clear
+//        containerView.backgroundColor = .orange
         containerView.alpha = 0
         addSubview(containerView)
         
         let picButton = UIButton(type: .system)
-        picButton.frame = CGRect(x: 20, y: leftButton.frame.origin.y+leftButton.frame.size.height+30, width: 50, height: 50)
+        picButton.frame = CGRect(x: 20, y: 15, width: 50, height: 50)
         picButton.setImage(UIImage(named: "sharemore_pic"), for: .normal)
         picButton.addTarget(self, action: #selector(action1(_:)), for: .touchUpInside)
         
@@ -92,7 +117,7 @@ public class BWComposerView: UIView {
         if #available(iOS 13.0, *) {
             let dyColor1 = UIColor { (trainCollection) -> UIColor in
                 if trainCollection.userInterfaceStyle == .light {
-                    return UIColor.white
+                    return UIColor(red: 250/255.0, green: 250/255.0, blue: 250/255.0, alpha: 1)
                 } else {
                     return UIColor.tertiarySystemBackground
                 }
@@ -102,31 +127,61 @@ public class BWComposerView: UIView {
             
             let dyColor2 = UIColor { (trainCollection) -> UIColor in
                 if trainCollection.userInterfaceStyle == .light {
+                    return UIColor.white
+                } else {
+                    return UIColor.secondarySystemBackground
+                }
+            }
+            pressMic.backgroundColor = dyColor2
+            
+            let dyColor3 = UIColor { (trainCollection) -> UIColor in
+                if trainCollection.userInterfaceStyle == .light {
                     return UIColor(red: 243/255.0, green: 244/255.0, blue: 245/255.0, alpha: 1)
                 } else {
                     return UIColor.systemBackground
                 }
             }
-            textView.backgroundColor = dyColor2
+            textView.backgroundColor = dyColor3
             
         } else {
             // Fallback on earlier versions
-            self.backgroundColor = UIColor.white
-            backgroundView.backgroundColor = UIColor.white
+            self.backgroundColor = UIColor(red: 250/255.0, green: 250/255.0, blue: 250/255.0, alpha: 1)
+            backgroundView.backgroundColor = UIColor(red: 250/255.0, green: 250/255.0, blue: 250/255.0, alpha: 1)
+            pressMic.backgroundColor = UIColor.white
             textView.backgroundColor = UIColor(red: 243/255.0, green: 244/255.0, blue: 245/255.0, alpha: 1)
         }
-        
-        //
-        textView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
     }
     
     @objc func action1(_ button: UIButton) {
         if keyboardStatus != .BWLeft {
+            leftButton.setImage(UIImage(named: "ToolViewKeyboard"), for: .normal)
+            kLastTextViewFrame = textView.frame
+            textView.isHidden = true
+            var frame = textView.frame
+            frame.size.height = 0
+            textView.frame = frame
+            
+            leftButton.frame.origin.y = 15
+            rightButton.frame.origin.y = 15
+            
             self.containerView.alpha = 0
             keyboardStatus = .BWLeft
             showArea = false
             textView.resignFirstResponder()
             delegate?.leftButtonAction(self)
+        }
+        else if keyboardStatus == .BWLeft {
+            leftButton.setImage(UIImage(named: "语音按钮"), for: .normal)
+            textView.frame = kLastTextViewFrame
+            textView.isHidden = false
+            textView.becomeFirstResponder()
+
+            leftButton.frame.origin.y = kLastTextViewFrame.size.height-30
+            rightButton.frame.origin.y = kLastTextViewFrame.size.height-30
+            
+            keyboardStatus = .BWEditing
+            showArea = false
+            delegate?.textViewBeginEdit(textView)
         }
     }
     
@@ -135,9 +190,67 @@ public class BWComposerView: UIView {
             UIView.animate(withDuration: 0.25) {
                 self.containerView.alpha = 1
             }
-            keyboardStatus = .BWRight
+            textView.isHidden = false
+            textView.frame = kLastTextViewFrame
             textView.resignFirstResponder()
+            
+            leftButton.frame.origin.y = kLastTextViewFrame.size.height-30
+            rightButton.frame.origin.y = kLastTextViewFrame.size.height-30
+            
+            keyboardStatus = .BWRight
             delegate?.rightButtonAction(self)
+        }
+        else if keyboardStatus == .BWRight {
+            textView.frame = kLastTextViewFrame
+            textView.isHidden = false
+            textView.becomeFirstResponder()
+
+            leftButton.frame.origin.y = kLastTextViewFrame.size.height-30
+            rightButton.frame.origin.y = kLastTextViewFrame.size.height-30
+            
+            keyboardStatus = .BWEditing
+            showArea = false
+            delegate?.textViewBeginEdit(textView)
+        }
+    }
+    
+    @objc private func longPressRecognized(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            pressMicLabel.text = "松开 结束"
+            if #available(iOS 13.0, *) {
+                let dyColor1 = UIColor { (trainCollection) -> UIColor in
+                    if trainCollection.userInterfaceStyle == .light {
+                        return UIColor(red: 246/255.0, green: 246/255.0, blue: 247/255.0, alpha: 1)
+                    } else {
+                        return UIColor.systemBackground
+                    }
+                }
+                pressMic.backgroundColor = dyColor1
+                
+            } else {
+                // Fallback on earlier versions
+                pressMic.backgroundColor = UIColor.white
+            }
+        }
+        else if sender.state == .changed {
+            
+        }
+        else {
+            pressMicLabel.text = "按住 说话"
+            if #available(iOS 13.0, *) {
+                let dyColor1 = UIColor { (trainCollection) -> UIColor in
+                    if trainCollection.userInterfaceStyle == .light {
+                        return UIColor.white
+                    } else {
+                        return UIColor.secondarySystemBackground
+                    }
+                }
+                pressMic.backgroundColor = dyColor1
+                
+            } else {
+                // Fallback on earlier versions
+                pressMic.backgroundColor = UIColor.white
+            }
         }
     }
     
@@ -156,12 +269,15 @@ public class BWComposerView: UIView {
     public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if object as AnyObject? === textView && keyPath == "contentSize" {
             let height: CGFloat = min(100, textView.contentSize.height <= kTextViewDefaultHeight ? kTextViewDefaultHeight : textView.contentSize.height)
-            print("abcd ComposerView observeValue textView \(textView.frame.size.height) \(textView.contentSize.height)")
+            print("abcd ComposerView observeValue textView \(textView.frame.size.height) \(textView.contentSize.height) \(height)")
             var frame = textView.frame
             frame.size.height = height
             textView.frame = frame
+            kLastTextViewFrame = frame
+            
             leftButton.frame.origin.y = height-30
             rightButton.frame.origin.y = height-30
+            containerView.frame.origin.y = leftButton.frame.origin.y+50
             delegate?.textViewTextChange(textView)
         }
         
@@ -170,7 +286,7 @@ public class BWComposerView: UIView {
             let rect = self.superview!.frame
             let height = UIScreen.main.bounds.height-rect.minY-self.frame.height
             if keyboardStatus != .BWRight {
-                delegate?.keyboardFrameChangeByDrag(textView, keyBoardheight: height)
+                delegate?.keyboardFrameChange(textView, keyBoardheight: height)
             }
             print("observeValue self.superview!.frame.origin.y \(rect.origin.y) \(height)")
             #endif
